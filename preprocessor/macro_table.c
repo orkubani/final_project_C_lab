5ce976e72ab14b3ba49f4d58d22c13adadc19a31
Or Kubani <orkubani@gmail.com>
2023-07-27 20:44:29 +0300

    Implement insert_macro_line

--- preprocessor/macro_table.c
+++ preprocessor/macro_table.c
@@ -32,7 +32,38 @@ Macro create_macro(char macro_name[MAX_LINE_LENGTH])
 {
     Macro macro;
     strcpy(macro.name, macro_name);
-    macro.lines = NULL;
+    macro.lines = calloc(1, sizeof(char**));
     macro.num_of_lines = 0;
     return macro;
 }
+
+
+void insert_macro_line(Macro *macro, const char *line)
+{
+    macro->lines[macro->num_of_lines] = calloc(1, sizeof(char *));
+    if (macro->lines[macro->num_of_lines] == NULL)
+    {
+        printf("Memory allocation failed.\n");
+        /*TODO Check if needed to return NULL*/
+        exit(0);
+    }
+    strcpy(macro->lines[macro->num_of_lines], line);
+    macro->num_of_lines += 1;
+}
+
+
+/* Tester 
+int main()
+{    
+    int i;
+    Macro first_macro;
+    first_macro = create_macro("First Macro");
+    insert_macro_line(&first_macro, "Test1");
+    insert_macro_line(&first_macro, "Test2");
+    for (i=0;i<first_macro.num_of_lines;i++)
+    {
+        printf("%s\n",first_macro.lines[i]);
+    }
+    
+    return 0; 
+} */
