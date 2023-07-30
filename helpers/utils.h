693198888018ecb378622b65028aede7ed965dfa
Or Kubani <orkubani@gmail.com>
2023-07-30 19:29:45 +0300

    Fixed file name size for debug

--- helpers/utils.h
+++ helpers/utils.h
@@ -3,7 +3,7 @@
 
 #define COMMENT_PREFIX ';'
 #define MAX_LINE_LENGTH 81
-#define MAX_FILE_NAME_LENGTH 31
+#define MAX_FILE_NAME_LENGTH 200
 
 void remove_white_spaces(char *source_line, char *dest_line);
 void remove_prefix_white_spaces(char *source_line, char *dest_line);
