39ffded7256cbf61c0a76f788b623a0a41c1c808
Or Kubani <orkubani@gmail.com>
2023-08-01 21:21:08 +0300

    Finish implementation of analyzed_line struct

---
+++ analyzer/analyzer.h
@@ -0,0 +1,96 @@
+#ifndef ANALYZER_H
+#define ANALYZER_H
+
+#include "../helpers/utils.h"
+
+struct analyzed_line
+{
+    char label_name[MAX_LABEL_LENGTH]; 
+
+    /* Only one of the following can be the line type. Blank and Comment lines are in the preprocessor responsibility. */
+    enum
+    {
+        directive,
+        instruction
+    }analyzed_line_opt;
+
+    union
+    {
+        struct /* Directive */
+        {
+            enum /* Four kinds of directive */
+            {
+                extern_dir,
+                entry_dir,
+                string_dir,
+                data_dir
+
+            }dir_opt;
+
+            union
+            {
+                char * label_name; /* .entry or .extern */
+                char * string; /* .string */
+                struct /* .data */
+                {
+                    int data[MAX_LINE_LENGTH];
+                    int data_count;
+                }data;
+
+            }dir_operand;
+
+        }directive; /* End of Directive */
+
+        struct /* Instruction */
+        {
+            enum /* 16 opcodes */
+            {
+                /* Require 2 operands */
+                inst_mov,
+                inst_cmp,
+                inst_add,
+                inst_sub,
+                inst_lea,
+
+                /* Require 1 operand */
+                inst_not,
+                inst_clr,
+                inst_inc,
+                inst_dec,
+                inst_jmp,
+                inst_bne,
+                inst_red,
+                inst_prn,
+                inst_jsr,
+
+                /* Without operands */
+                inst_rts,
+                inst_stop
+
+            }inst_opt;
+
+            enum /* Addressing mode */
+            {
+                operand_none = 0,
+                operand_const_number = 1,
+                operand_label = 3,
+                operand_register = 5
+
+            }inst_operand_options;
+
+            union
+            {
+                int const_number;
+                int register_number;
+                char * label;
+
+            }inst_operands[2]; /* 2 for source and dest */
+
+        }instruction; /* End of Instruction */
+
+    }dir_or_inst;
+};
+
+typedef struct analyzed_line analyzed_line;
+
+#endif /* ANALYZER_H */
