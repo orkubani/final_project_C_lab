33fa5c1135d6fce2c631fdf71030d35b1882fcfa
Or Kubani <orkubani@gmail.com>
2023-08-02 20:37:50 +0300

    Start is_dir_or_inst

--- analyzer/analyzer.c
+++ analyzer/analyzer.c
@@ -1,19 +1,39 @@
 #include "analyzer.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+int is_syntax_error(char *line)
+{
+    /* return 1 if there is a syntax error */
+    /* return 0 if there is no syntax error */ 
+}
 
- int is_dir_or_inst(char *line)
- {
-    /* Default for testing */
-    return DIR_ENUM_CODE;
- }
+int is_dir_or_inst(char *line)
+{
+    int i;
+    char clean_line[MAX_LINE_LENGTH];
+    remove_white_spaces(line, clean_line);
 
+    /* Option 1 when line is a directive. entry/extern */
+    if (clean_line[0] == '.')
+        return DIR_ENUM_CODE;
+    
+    /* Option 2 when line is a directive. label */
+    for(i = 0; i < strlen(clean_line) - 1; i++)
+    {
+        if (clean_line[i] == ':' && clean_line[i + 1] == '.') 
+        {
+            return DIR_ENUM_CODE;
+        }
+    }
+
+    return INST_ENUM_CODE;
+}
 
 Analyzed_line get_analyzed_line(char *line)
 {
     Analyzed_line analyzed_line;
-    int line_type;
 
     asm_instruction asm_all_instructions[NUM_OF_INST] = 
     {
@@ -52,7 +72,13 @@ Analyzed_line get_analyzed_line(char *line)
     {"entry", dir_entry},
     };
 
-    /* Check if the line is a directive or an instruction and change the analyzed_line according to the result. */
+    if (is_syntax_error(line))
+    {
+        strcpy(analyzed_line.syntax_error, "Syntax Error was detected");
+        return analyzed_line;
+    }
+
+    /* Check line type */
     if (is_dir_or_inst(line) == DIR_ENUM_CODE)
         analyzed_line.analyzed_line_opt = directive;
     else
@@ -62,10 +88,26 @@ Analyzed_line get_analyzed_line(char *line)
 }
 
 
-int main()
+int main(int argc, char** argv)
 {
-    Analyzed_line result;
-    char input_line[] = "bla";
-    result = get_analyzed_line(input_line);
+    FILE * analyzer_input_test;
+    Analyzed_line analyzed_line_result;
+    char line[MAX_LINE_LENGTH];
+
+    analyzer_input_test = fopen(argv[1], "r");
+    if (analyzer_input_test == NULL) 
+    {
+        printf("Error opening the analyzer_input_test file.\n");
+        return 0;
+    }
+
+    /* Analyze each line and perform the relevant action */
+    while (fgets(line, MAX_LINE_LENGTH, analyzer_input_test) != NULL) 
+    {
+        analyzed_line_result = get_analyzed_line(line);
+        printf("Line: %s", line);
+        printf("Line Type (0 - directive | 1 instruction): %d\n\n", analyzed_line_result.analyzed_line_opt);
+    }
+
     return 0;
 }
