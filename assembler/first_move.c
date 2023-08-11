766edf03fc715a0b3282bdc640d66ee14e50acd1
Or Kubani <orkubani@gmail.com>
2023-08-11 15:42:50 +0300

    Start implement "first_move"

--- assembler/first_move.c
+++ assembler/first_move.c
@@ -1,3 +1,42 @@
 #include "first_move.h"
 #include <stdio.h>
 #include <stdlib.h>
+
+int first_move(FILE * am_file, Object_File * object_file, const char * am_filename)
+{
+    char line[MAX_LINE_LENGTH] = {0}; /* Initilize line */
+    Analyzed_line analyzed_line;
+    Compiled_Line * data_section;
+    Compiled_Line * code_section;
+    int line_index = 1;
+    unsigned int word = 0;
+
+    while (fgets(line, sizeof(line), am_file)) 
+    {
+        analyzed_line = get_analyzed_line(line);
+
+        if (is_valid_analyzed_line(&analyzed_line) == FALSE)
+        {
+            printf("Error in line '%d'! %s\n", line_index, analyzed_line.syntax_error);
+            line_index++;
+            continue;
+        }
+
+        /* Handle Main Label Here */
+
+        if (analyzed_line.analyzed_line_opt == directive)
+        {
+            /* Implement Here */
+            return TRUE;
+        }
+
+        else if (analyzed_line.analyzed_line_opt == instruction) 
+        {
+            /* Implement Here */
+            return TRUE;
+        }
+    } 
+
+    /* First Move failed */
+    return FALSE;
+}
--- assembler/first_move.h
+++ assembler/first_move.h
@@ -1,10 +1,27 @@
 #ifndef FIRST_MOVE_H
 #define FIRST_MOVE_H
 
+#include "../helpers/utils.h"
+#include "../analyzer/analyzer.h"
+#include "../helpers/analyzer_helper.h"
 #include "../data_structures/compiled_line.h"
+#include <stdio.h>
 
 #define BASE_ADDRESS 100
 
+typedef struct Object_File 
+{
 
+}Object_File;
 
+/**
+ * @brief First move on the am_file.
+ * 
+ * @param am_file Pointer to the am_file.
+ * @param object_file Pointer to a struct thats hold the relevant data for the object file.
+ * @param am_filename The am filename.
+ *
+ * @return TRUE (1) if there are no errors, FALSE (0) if there are errors and the process failed
+ */
+int first_move(FILE * am_file, Object_File * object_file, const char * am_filename);
 #endif /* FIRST_MOVE_H */
