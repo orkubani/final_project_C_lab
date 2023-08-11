8b9b5a396697c016a27d4c15bce1be1bf2752009
Or Kubani <orkubani@gmail.com>
2023-08-11 14:52:15 +0300

    Create "insert_compiled_line_to_table"

--- assembler/first_move.c
+++ assembler/first_move.c
@@ -4,7 +4,7 @@
 
 
 /* Create "compiled instruction" / "compile directive" Node */
-Compiled_Line * create_compiled_line()
+Compiled_Line * create_compiled_line(int line_index)
 {
     Compiled_Line * compiled_inst = NULL;
     compiled_inst = (Compiled_Line *)calloc(1, sizeof(Compiled_Line));
@@ -14,10 +14,31 @@ Compiled_Line * create_compiled_line()
         exit(0);
     }
 
+    compiled_inst->line_index = line_index;
     compiled_inst->dir_words = NULL;
     compiled_inst->num_of_words = 0;
     compiled_inst->next_compiled_line = NULL;
 
     return compiled_inst;
+}
+
+/* Inserts a new compiled_line with the given line_index into the compiled_line. */
+Compiled_Line * insert_compiled_line_to_table(Compiled_Line *compiled_line, int line_index)
+{
+   if (compiled_line == NULL) 
+   {
+        compiled_line = create_compiled_line(line_index);
+        return compiled_line;
+   }
+
+   else if (compiled_line->next_compiled_line == NULL) 
+   {
+        compiled_line->next_compiled_line = create_compiled_line(line_index);
+        compiled_line->next_compiled_line->next_compiled_line = NULL;
+        return compiled_line;
+   } 
+   
+   insert_compiled_line_to_table(compiled_line->next_compiled_line, line_index);
+   return compiled_line;
 }
 
--- assembler/first_move.h
+++ assembler/first_move.h
@@ -6,6 +6,7 @@
 /* "Instruction" / "Directive" Compiled Line */
 typedef struct Compiled_Line /* Code section */
 {
+    int line_index;
     unsigned int ** dir_words;
     int num_of_words;
     struct Compiled_Line *next_compiled_line;
@@ -24,9 +25,19 @@ typedef struct Entries_Calls
 
 /**
  * @brief Create "compiled instruction" / "compile directive" Node.
- *
+ * 
+ * @param line_index The line index from the am_file.
  * @return Pointer to the compiled_line Node.
  */
-Compiled_Line * create_compiled_line();
+Compiled_Line * create_compiled_line(int line_index);
+
+/**
+ * @brief Inserts a new compiled_line with the given line_index into the compiled_line.
+ * 
+ * @param line_index The line index from the am_file.
+ * @param compiled_line Pointer to the compiled_line table
+ * @return Pointer to the compiled_line Node.
+ */
+Compiled_Line * insert_compiled_line_to_table(Compiled_Line *compiled_line, int line_index);
 
 #endif /* FIRST_MOVE_H */
