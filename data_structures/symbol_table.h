19e34f8d9ee6de003838e3e5003ed6c61cd05cef
Or Kubani <orkubani@gmail.com>
2023-08-12 16:13:16 +0300

    Fix Symbol struct

--- data_structures/symbol_table.h
+++ data_structures/symbol_table.h
@@ -10,14 +10,19 @@ typedef struct Symbol
     unsigned int address;
     enum
     {
-        symbol_code,
-        symbol_data,
-        symbol_extern,
+        symbol_local_code,
+        symbol_local_data,
+
+        symbol_extern_def,
+        symbol_extern_code,
+        symbol_extern_data,
+
         symbol_entry_def,
         symbol_entry_code,
         symbol_entry_data
     };
 	struct Symbol *next_symbol;
+
 } Symbol;
 
 #endif /* SYMBOL_TABLE_H */
